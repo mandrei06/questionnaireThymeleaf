290b58df51894802217f84e3449f0492075d2928
Andy <54832870+mandrei06@users.noreply.github.com>
2022-11-07 14:12:29 +0100

    Get email

--- src/main/java/com/questionnaireApp/questionnaireApp/Controller/UserController.java
+++ src/main/java/com/questionnaireApp/questionnaireApp/Controller/UserController.java
@@ -4,17 +4,20 @@ import com.questionnaireApp.questionnaireApp.model.User;
 import com.questionnaireApp.questionnaireApp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class UserController {
     @Autowired
     UserRepo userRepo;
 
+
     @PostMapping("/addUser")
-    public String getUserEmail(@ModelAttribute ("userToAdd") User userToAdd){
-        userRepo.save(userToAdd);
+    public String getUserEmail(@RequestParam String email){
+        System.out.println("User email:"+email);
+        User user=new User(email);
+        userRepo.save(user);
         return "questions";
     }
 }
