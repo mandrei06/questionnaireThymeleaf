8d07913834bff1469b87bd9a88d509147209be05
Petros Grivas <petros.grivas@solera.com>
2022-11-07 17:02:16 +0100

    Html

--- src/main/java/com/questionnaireApp/questionnaireApp/Controller/UserController.java
+++ src/main/java/com/questionnaireApp/questionnaireApp/Controller/UserController.java
@@ -1,7 +1,9 @@
 package com.questionnaireApp.questionnaireApp.Controller;
 
+import com.questionnaireApp.questionnaireApp.model.Answer;
 import com.questionnaireApp.questionnaireApp.model.Question;
 import com.questionnaireApp.questionnaireApp.model.User;
+import com.questionnaireApp.questionnaireApp.repository.AnswerRepo;
 import com.questionnaireApp.questionnaireApp.repository.QuestionRepo;
 import com.questionnaireApp.questionnaireApp.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +21,36 @@ public class UserController {
 
     @Autowired
     QuestionRepo questionRepo;
-
+    @Autowired
+    AnswerRepo answerRepo;
+    List<Question> questionsList;
     int i=1;
+
+    Integer userId;
+    int maxSize;
     @PostMapping("/addUser")
     public String getUserEmail(@RequestParam String email, Model model){
         System.out.println("User email:"+email);
         User user=new User(email);
         userRepo.save(user);
-        List<Question>  questionsList=questionRepo.findAll();
-        System.out.println(questionsList);
+        questionsList=questionRepo.findAll();
+        maxSize = questionsList.size();
+        userId = user.getUserId();
         model.addAttribute("questionList",questionsList.get(i));
         i++;
+        return "questions";
+    }
+    @PostMapping("/answerQuestion")
+    public String getAnswer(@RequestParam String answer, Model model){
+        Answer answer1 = new Answer(i-1,userId,answer);
+        answerRepo.save(answer1);
+        if(i<=maxSize-1)
+            model.addAttribute("questionList",questionsList.get(i));
+        i++;
+        if (i==maxSize+1){
+            return "end";
+        }
+
         return "questions";
     }
 }
--- src/main/java/com/questionnaireApp/questionnaireApp/model/Answer.java
+++ src/main/java/com/questionnaireApp/questionnaireApp/model/Answer.java
@@ -1,6 +1,8 @@
 package com.questionnaireApp.questionnaireApp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,10 +15,20 @@ import lombok.NoArgsConstructor;
 public class Answer {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer answerId;
     private Integer questionId;
     private Integer userId;
     private String answer;
+
+    public Answer(Integer questionId, Integer userId, String answer) {
+        this.questionId = questionId;
+        this.userId = userId;
+        this.answer = answer;
+    }
+
+
+
 }
 
 
