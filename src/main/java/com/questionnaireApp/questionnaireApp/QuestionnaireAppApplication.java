ee4f98a2d4aed864e412c51157ecfd9e3d125aec
Petros Grivas <petros.grivas@solera.com>
2022-11-07 09:45:47 +0100

    Initial Commit

---
+++ src/main/java/com/questionnaireApp/questionnaireApp/QuestionnaireAppApplication.java
@@ -0,0 +1,13 @@
+package com.questionnaireApp.questionnaireApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireAppApplication.class, args);
+	}
+
+}
